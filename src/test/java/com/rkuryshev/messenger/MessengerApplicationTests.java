46d93a2b4b7135bc045549025417730d46e65f6a
romankuryshev <romankuryshev1406@gmail.com>
2023-06-27 20:13:59 +0300

    create

---
+++ src/test/java/com/rkuryshev/messenger/MessengerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rkuryshev.messenger;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MessengerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
